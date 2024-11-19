2cc739eb26575621401a6a28fae6c2e2e05e3c97
Anton Amarjargal <anton@Antons-MacBook-Pro.local>
2024-11-19 11:29:57 +0800

    js

---
+++ first.js
@@ -0,0 +1,42 @@
+// let x = 11;
+// let y = 20;
+
+// x = y;
+
+// let niilber = x + y;
+
+// console.log(niilber);
+
+// let dorom = true;
+// let durem = 18;
+
+// if (dorom == true) {
+//   if (dorom > 17) {
+//     console.log("tentssen");
+//   } else {
+//     console.log("durem tenteseegui");
+//   }
+// } else {
+//   if (dorom < 17) {
+//     console.log("tentseegui");
+//   }
+// }
+
+if (score > 17) {
+  // onoo maan 17s ih
+  if (dorom == true) {
+    console.log("tentssen");
+  } else {
+    console.log("dorom tentseh heregtei");
+  }
+} else {
+  // onoo maan 17s bag
+  if (dorom == true) {
+    console.log("testee davah heregtei");
+  } else {
+    console.log("2 uuland unsana baina");
+  }
+}
+
+let nas = 23;
+//
---
+++ second.js
@@ -0,0 +1,26 @@
+// let too = 123321;
+// let uldegdeel = too % 2;
+// if (uldegdeel == 0) {
+//   console.log("tegsh too");
+// } else {
+//   console.log("sondgoi too");
+// }
+
+// too 7d huvaagdana 11ch huvaagdana
+// too 7d huvaagdana 11d huvaagdahgui
+// too 7d huvaagdahgui 11d huvaagdana
+// too 7d huvaagdahgui 11ch huvaagdahgui
+
+let too1 = 22;
+if (too1 % 7 == 0) {
+  if (too1 % 11 == 0) {
+    console.log("too 7d huvaagdana 11ch huvaagdana");
+  } else {
+    console.log("too 7d huvaagdana 11d huvaagdahgui");
+  }
+} else {
+  if (too1 % 11 == 0) console.log("too 7d huvaagdahgui 11d huvaagdana");
+  else {
+    console.log("too 7d huvaagdahgui 11ch huvaagdahgui");
+  }
+}
